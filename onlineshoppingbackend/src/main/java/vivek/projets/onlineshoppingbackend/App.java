80a3d01de5be5e5a6761802a7155f74ce09b754e
vivek <thevivek2601s@gmail.com>
2018-03-13 12:05:54 -0700

    My project setup completed successfuly

---
+++ onlineshoppingbackend/src/main/java/vivek/projets/onlineshoppingbackend/App.java
@@ -0,0 +1,13 @@
+package vivek.projets.onlineshoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
